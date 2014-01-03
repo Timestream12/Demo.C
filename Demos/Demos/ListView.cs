953ef7a4038ebe98a639188b5699c4264ed60ba1
Hak Soo Kim <timestream12@gmail.com>
2014-01-03 09:30:18 -0500

    added files back

---
+++ Demos/Demos/ListView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demos
+{
+    public partial class ListView : Form
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem lvi = new ListViewItem(textBox1.Text);
+            lvi.SubItems.Add(textBox2.Text);
+            lvi.SubItems.Add(textBox3.Text);
+            listView1.Items.Add(lvi);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void getNameOfItemToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 0)
+            {
+                MessageBox.Show(listView1.SelectedItems[0].SubItems[0].Text);
+            }
+        }
+    }
+}
