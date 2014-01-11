b511d09cd6df751c3aff75946f0dcaf888392dd7
Hak Soo Kim <timestream12@gmail.com>
2014-01-10 19:17:59 -0500

    tut 139 Inheriting from existing control

---
+++ Demos/Demos/UserControl1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demos
+{
+    public partial class UserControl1 : Button
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        public override string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+                if (value == "XYX")
+                {
+                    MessageBox.Show("XYZ");
+                    base.Text = "XYZ";
+                    return;
+                }
+                base.Text = value;
+            }
+        }
+        protected override void OnClick(EventArgs e)
+        {
+            //MessageBox.Show("");
+            base.OnClick(e);
+        }
+    }
+}
